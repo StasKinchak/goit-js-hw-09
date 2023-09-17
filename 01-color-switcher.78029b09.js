d18b553d0afac90a12cb0ec686c69bba9949d756
StasKinchak <130861052+StasKinchak@users.noreply.github.com>
2023-09-17 13:35:37 +0000

    Deploying to gh-pages from @ StasKinchak/goit-js-hw-09@3109d9342c945570483e584a2fc4da560572b776 🚀

---
+++ 01-color-switcher.78029b09.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll("[data-start]"),t=document.querySelectorAll("[data-stop]"),n=(document.querySelector("body"),null);e.addEventListener("click",(function(){n=setInterval((function(){}),1e3)})),t.addEventListener("click",(function(){clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.78029b09.js.map
