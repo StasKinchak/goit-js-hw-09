3109d9342c945570483e584a2fc4da560572b776
StasKinchak <130861052+StasKinchak@users.noreply.github.com>
2023-09-17 15:35:06 +0200

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,8 @@
-const startBtn = document.querySelectorAll('[data-start]'); button.addEventListener('click', startColorChange);
-const stopBtn = document.querySelectorAll('[data-stop]');
-
-startBtn = button.addEventListener('click', startColorChange);
-stopBtn= button.addEventListener('click', stopColorChange);
-
-
-        let intervalId; 
-        let isChanging = false; 
-
+    const startBtn = document.querySelectorAll('[data-start]');
+    const stopBtn = document.querySelectorAll("[data-stop]");
+    const bodySite = document.querySelector("body");
+    
+    let timerId = null; 
     
         function getRandomHexColor() {
             return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
@@ -18,25 +13,22 @@ stopBtn= button.addEventListener('click', stopColorChange);
             document.body.style.backgroundColor = getRandomHexColor();
         }
 
-    
-        function startColorChange() {
-            if (!isChanging) {
-                intervalId = setInterval(changeBackgroundColor, 1000);
-                isChanging = true;
-                document.querySelectorAll('[data-start]').forEach(button => button.disabled = true);
-                document.querySelectorAll('[data-stop]').forEach(button => button.disabled = false);
-            }
-        }
-
         
-        function stopColorChange() {
-            if (isChanging) {
-                clearInterval(intervalId);
-                isChanging = false;
-                document.querySelectorAll('[data-start]').forEach(button => button.disabled = false);
-                document.querySelectorAll('[data-stop]').forEach(button => button.disabled = true);
-            }
-        }
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    changeBackgroundColor;
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  
+});
+
+    
+        
 
    
     
