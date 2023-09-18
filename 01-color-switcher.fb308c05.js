b2a92cedfc69ff9a9dfba539c7b2d0e67f8f6019
StasKinchak <130861052+StasKinchak@users.noreply.github.com>
2023-09-18 06:11:55 +0000

    Deploying to gh-pages from @ StasKinchak/goit-js-hw-09@c306136d77694986871d4b0afe5de2c6ac1bb6a7 🚀

--- 01-color-switcher.fb308c05.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelectorAll("button[data-stop]"),d=document.querySelector("body");function n(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n(),intervalId=setInterval(n,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(intervalId)}));
-//# sourceMappingURL=01-color-switcher.fb308c05.js.map
